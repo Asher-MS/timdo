18e78ff195e6748f7f5d10622471e12ba36b02d4
Asher Mathews Shaji <msasher123@gmail.com>
2021-07-18 16:33:57 +0530

    Added the Speed reader
    Added the speereader.

--- src/components/SpeedReader.js
+++ src/components/SpeedReader.js
@@ -3,14 +3,35 @@ import React,{useState,useEffect} from 'react'
 import axios from 'axios';
 import { Play,Pause,PlayFill,PauseFill, Columns } from '@geist-ui/react-icons'
 
-
+let speedReadingInterval;
 function SpeedReader(props){
 
-    let [currentWord,setCurrentWord]=useState("Test");
+    let [currentWord,setCurrentWord]=useState("");
     let [isPlaying,setPlaying]=useState(false);
     let [matter,setMatter]=useState("Speed reading is an effective way to increase your learning speed");
+    let matterWords=[];
+    let currentWordIndex=0;
+    let wordUpdater=function(){
+        setCurrentWord(window.matterWords[currentWordIndex]);
+        currentWordIndex++;
+        if(currentWordIndex>window.matterWords.length){
+            currentWordIndex=0;
+        }
+        
+    }
+    
+    let matterSplit=function(matter){
+        window.matterWords=matter.split(" ");
+    }
     let play=function(){
         setPlaying(!isPlaying);
+        
+        if(!isPlaying){
+            window.speedReadingInterval=setInterval(()=>{wordUpdater()},200);
+        }else{
+            
+            clearInterval(window.speedReadingInterval);
+        }
 
 
     }
@@ -28,7 +49,7 @@ function SpeedReader(props){
             </Row> 
             <Card hoverable>
             <Row justify='center'>
-            <Text>{currentWord}</Text>
+            <Text h2>{currentWord}</Text>
             </Row>
             </Card>
             <Spacer y={1}/>
@@ -38,8 +59,8 @@ function SpeedReader(props){
             </Row>
             <Spacer y={1}/>
             <Col>
-            <Text h6>Enter the text to read here</Text>
-            <Textarea width="100%" placeholder="Speed reading is an effective way to increase your learning speed" onChange={(e)=>{setMatter(e.target.value)}}/>
+            <Text h6>Enter the text here</Text>
+            <Textarea width="100%" placeholder="Speed reading is an effective way to increase your learning speed" onChange={(e)=>{setMatter(e.target.value);matterSplit(e.target.value)}}/>
             </Col>
             
        
